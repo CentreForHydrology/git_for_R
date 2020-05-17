007b3e2a08430f5c5d32d91fd12b50ad44b7631d
rchlumsk <rchlumsk@uwaterloo.ca>
2020-05-16 21:32:27 -0400

    Added installation script into setup folder

---
+++ setup/install_R_packages_gitwebinar.R
@@ -0,0 +1,20 @@
+
+
+## install required packages for workshop ----
+pkgs <- c("rmarkdown","knitr","ggplot2","devtools")
+install.packages(pkgs)
+
+
+
+## install CSHS-hydRology package ----
+library(devtools)
+install_github("CSHS-CWRA/CSHShydRology")
+
+## note: if asked to install updated versions of packages, press 3 to skip updates or 2 to update all CRAN packages
+
+
+
+## check if git is installed ----
+# if not installed, will say something like:
+# "'git' is not recognized as an internal or external command, operable program or batch file."
+system("git --version")
